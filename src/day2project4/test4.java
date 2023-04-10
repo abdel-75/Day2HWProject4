b540a3fcd42e2269356e7a341ecdbd2104d922ca
Abdessalam <Abdessalam@192.168.148.117>
2023-04-10 16:37:14 +0000

    second commit

--- src/day2project4/test4.java
+++ src/day2project4/test4.java
@@ -7,4 +7,18 @@ public class test4 {
 		System.out.println("this is test 4");
 		
 	}
+	
+	public void getLocation( String country , String state) {
+		
+		System.out.println(" the Country name  is :" + country);
+		System.out.println(" the state name  is :" + state);
+		
+	}
+	
+	public void getMoreLocationInfo( String city , int zipCode) {
+		
+		System.out.println(" the city name  is :" + city);
+		System.out.println(" the  zip cood   is :" + zipCode);
+	}
+	
 }
